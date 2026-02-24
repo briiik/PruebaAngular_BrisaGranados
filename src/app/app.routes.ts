9f2119d7a91f44973be830c3e3bb537190d5a2ce
briiik <brisa.granados158@gmail.com>
2026-02-23 23:20:24 -0600

    Practica 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../app/pages/home/home.component';
+import { LoginComponent } from '../app/pages/login/login.component';
+import { RegisterComponent } from '../app/pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+];
