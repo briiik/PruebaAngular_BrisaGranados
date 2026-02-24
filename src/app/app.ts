c5713c4e6c53e9532b4ebd6394298a09a6f38ea7
briiik <brisa.granados158@gmail.com>
2026-02-23 22:59:01 -0600

    Practica 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
-export class App {
-  protected readonly title = signal('ERP');
+export class AppComponent {
+  title = 'mi-proyecto';
 }
